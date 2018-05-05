d98d5c8e98760b965ff65ff63eb3f091b4c4b781
David Perez <david.perez@us.abb.com>
2018-05-05 11:11:39 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+// backend
+// Dependencies
+// =============================================================
+let express = require("express");
+let bodyParser = require("body-parser");
+let path = require("path");
+
+let app = express();
+let PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var reservation = [
+    {
+        name: "firstReservation",
+        phoneNumber: "8139526965",
+        email: "daverioverde@gmail.com",
+        UniqueID: 1
+    },
+    {
+        name: "secondReservation",
+        phoneNumber: "6781239856",
+        email: "localhost@google.com",
+        UniqueID: 2
+    },
+    {
+        name: "thirdReservation",
+        phoneNumber: "4042341000",
+        email: "localhost@facebook.com",
+        UniqueID: 3
+    }
+];
+
+//identify routes for each request
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+//GET METHOD
+//go to your home page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+//make a new reservation
+app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+//display all the resevations
+app.get("/table", (req, res) => {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+//resturn all the reservation data
+app.get("/api/table", (req, res) => {
+    return res.json(reservations);
+});
+
+//POST METHOD
+//create a reservation data
+app.post("/api/reserve", (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newreservation = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newreservation.name = newreservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newreservation);
+
+    reservations.push(newreservation);
+
+    res.json(newreservation);
+});
+
+//listening port in the server.
+app.listen(PORT, () => {
+    console.log(`Server started on port `+ PORT);
+});
