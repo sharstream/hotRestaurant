2d9c735639955edc50cbb3a32d3ba4fa60a877cf
David Perez <david.perez@us.abb.com>
2018-05-06 10:33:09 -0400

    making reservation and displaying tables

--- public/app.js
+++ public/app.js
@@ -1,8 +1,13 @@
 // Question: What does this code do??
 $(document).ready(function(){
+
+    // In this code below we create the Front-end Javascript which "POSTS" our form data to our express server.
+    // In essence, when the user hits submit, jQuery grabs all of the fields then sends a post request to our api
+    // Our api recognizes the route (/api/tables)... and then runs the associated code (found in api-routes.js).
+    // In this case the associated code "saves" the data to the table-data.js file or waitinglist-data.js file
+
     //event handlers in jquery
     //submit a reservation
-
     $('#submit').on("click", function(event){
         console.log('Request received: ');
         event.preventDefault();
@@ -22,7 +27,19 @@ $(document).ready(function(){
             success: (data) => {
                 var ret = jQuery.parseJSON(data);
                 $('#lblResponse').html(ret.msg);
-                console.log('Success: ')
+                if (data == true) {
+                    alert("Yay! You are officially booked!")
+                }
+                // If a table is available... tell user they on the waiting list.
+                if (data == false) {
+                    alert("Sorry you are on the wait list")
+                }
+                //clear reservation fields
+                console.log(JSON.stringify(newreservation));
+                $('#name').val().text('');
+                $('#number').val().text('');
+                $('#email').val().text('');
+                $('#id').val().text('');
             },
             error: (xhr, status, error) => {
                 console.log('Error: ' + error.message);
@@ -30,14 +47,92 @@ $(document).ready(function(){
             }
         });
         //write the logic to submit the reservation to the back-end
-        console.log(JSON.stringify(newreservation));
-        $('#name').val().text('');
-        $('#number').val().text('');
-        $('#email').val().text('');
-        $('#id').val().text('');
     });
-    //display reservation
 
+    // In this code, jQuery is used to "download" the data from our server
+    // We then dynamically display this content in our table. This is very similar to the group projects you just completed.
+    // It's also very similar to the NYT search application. In fact, I copied a ton of code from there.
+
+    //display reservation
+    function runTableQuery() {
+        // Here we get the location of the root page.
+        // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+        var currentURL = window.location.origin;
+        // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+        $.ajax({
+                url: currentURL + "/api/tables",
+                method: "GET"
+            })
+            .done(function (tableData) {
+                // Here we are logging the URL so we have access to it for troubleshooting
+                console.log("------------------------------------");
+                console.log("URL: " + currentURL + "/api/tables");
+                console.log("------------------------------------");
+                // Here we then log the NYTData to console, where it will show up as an object.
+                console.log(tableData);
+                console.log("------------------------------------")
+                // Loop through and display each of the customers
+                for (var i = 0; i < tableData.length; i++) {
+                    // Create the HTML Well (Section) and Add the table content for each reserved table
+                    var tableSection = $("<div>");
+                    tableSection.addClass('well');
+                    tableSection.attr('id', 'tableWell-' + i + 1)
+                    $('#tableSection').append(tableSection);
+                    var tableNumber = i + 1;
+                    // Then display the remaining fields in the HTML (Section Name, Date, URL)
+                    $("#tableWell-" + i + 1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + tableData[i].customerID + "</h2>");
+                }
+            });
+    }
+
+    function runWaitListQuery() {
+        // Here we get the location of the root page.
+        // We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+        var currentURL = window.location.origin;
+        // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+        $.ajax({
+                url: currentURL + "/api/waitlist",
+                method: "GET"
+            })
+            .done(function (waitlistData) {
+                // Here we are logging the URL so we have access to it for troubleshooting
+                console.log("------------------------------------");
+                console.log("URL: " + currentURL + "/api/waitlist");
+                console.log("------------------------------------");
+                // Here we then log the NYTData to console, where it will show up as an object.
+                console.log(waitlistData);
+                console.log("------------------------------------")
+                // Loop through and display each of the customers
+                for (var i = 0; i < waitlistData.length; i++) {
+                    // Create the HTML Well (Section) and Add the table content for each reserved table
+                    var waitlistSection = $("<div>");
+                    waitlistSection.addClass('well');
+                    waitlistSection.attr('id', 'waitlistWell-' + i + 1)
+                    $('#waitlistSection').append(waitlistSection);
+                    var tableNumber = i + 1;
+                    // Then display the remaining fields in the HTML (Section Name, Date, URL)
+                    $("#waitlistWell-" + i + 1).append('<h2><span class="label label-primary">' + tableNumber + "</span> | " + waitlistData[i].customerID + "</h2>");
+                }
+            });
+    }
+    // This function resets all of the data in our tables. This is intended to let you restart a demo.
+    function clearTable() {
+        var currentURL = window.location.origin;
+        $.ajax({
+            url: currentURL + "/api/clear",
+            method: "POST"
+        })
+    }
+    $("#clear").on('click', function () {
+        alert("Clearing...");
+        clearTable();
+        // Refresh the page after data is cleared
+        location.reload();
+    })
+    // Run Queries!
+    // ==========================================
+    runTableQuery();
+    runWaitListQuery();
     //send message
 
     //
